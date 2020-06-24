ebdc7513ef56e09990eef97885b1b1f8b3e076f6
TJun-Jie <tjunjie2000@gmail.com>
2020-06-24 14:39:27 +0800

    Added first 2 quotes

--- js/script.js
+++ js/script.js
@@ -10,6 +10,26 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
+//Created empty quotes array to store Quotes(object)
+quotes = [];
+
+//Adding quotes
+quotes.push(
+  {quote: 'Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.',
+  source: 'Patrick McKenzie',
+  citation: 'Twitter',
+  year: '2016'}
+);
+
+quotes.push(
+  {quote: "Ignorance is the curse of God; knowledge is the wing wherewith we fly to heaven.",
+  source: "William Shakespeare",
+  citation: "Henri VI, Part 2",
+  year: "1591"}
+);
+
+
+
 
 
 
